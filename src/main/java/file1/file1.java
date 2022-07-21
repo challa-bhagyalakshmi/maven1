19734141e44d08442096c3d3f6d9065fdee608c6
challa-bhagyalakkshmi <challabhagyalakshmi2298@gmail.com>
2022-07-21 15:34:00 +0530

    added files

---
+++ src/main/java/file1/file1.java
@@ -0,0 +1,12 @@
+package file1;
+
+public class file1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("my second java project");
+
+	}
+
+}
